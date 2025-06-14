a3e233d6502ed7dc7b2e2132e3f5da333f18331d
1032231289 <nastasino20@gmail.com>
2025-06-14 16:21:18 +0300

    1

--- 1.py
+++ 1.py
@@ -1,4 +1,11 @@
+import pickle
+d='Hellod world'
 
+with open('data.txt', 'wb') as file:
+    pickle.dump(d, file)
+with open('data.txt', 'rb') as file:
+    print(file)
+    print(pickle.load(file))
 
 
 
